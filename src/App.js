f85c82423d1fbc190e854deb09337da5da52e58e
Benjamin Hurley <benfromtech@gmail.com>
2022-09-15 23:37:08 -0400

    add site header section

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useWindowSize } from './hooks'; 
 import './App.css';
 
+const headerTexture = require("./img/design/header-texture.png");
+const headerLogo = require("./img/design/header-logo.png");
 const divideTop = require("./img/design/divide-top.png");
 const divideBottom = require("./img/design/divide-bottom.png");
-const comingSoon = require("./img/design/comin-soon.png");
-const nuppy = require("./img/nippies/nuppy-transparent.png");
 
 function App() {
-  const isMobile = window?.innerWidth < 1024;
+  const [width, height] = useWindowSize();
+  const [isMobile, setIsMobile] = useState(window?.innerWidth < 1024);
 
-  const nuppyStyles = {
-    left: isMobile ? 0 : '15%',
-    bottom: isMobile ? '15px' : '30px',
+  const headerLogoStyles = {
+    maxWidth: isMobile ? 150 : 350,
+    marginLeft: isMobile ? -75 : -175,
+  };
+
+  const headerTextureStyles = {
+    maxHeight: isMobile ? 250 : 550,
+
   }
 
-  const comingSoonStyles = {
-    maxHeight: isMobile ? 150 : 275,
-  }
+  useEffect(() => {
+    setIsMobile(width < 1024);
+  }, [width])
 
   return (
     <div className="app">
+      <img src={headerTexture} className="header-texture" style={headerTextureStyles} alt="header texture"/>
+      <img src={headerLogo} className="header-logo" style={headerLogoStyles} alt="header logo"/>
       <img src={divideTop} className="app-divide-top" alt="nippies fur"/>
-      <img src={comingSoon} className="coming-soon" style={comingSoonStyles} alt="comin soon" />
-      <img src={nuppy} className="nuppy" style={nuppyStyles} alt="nuppy the nippie" />
       <img src={divideBottom} className="app-divide-bottom" alt="nippies fur"/>
     </div>
   );
