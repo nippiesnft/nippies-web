6dda7706b04c5e2ce6637427614b5debb4a4efb8
Benjamin Hurley <benfromtech@gmail.com>
2022-10-10 12:51:14 -0400

    more top margin above nuppy in wtf sectio

--- src/sections/wtfIsANippie/styles.js
+++ src/sections/wtfIsANippie/styles.js
@@ -10,7 +10,7 @@ position: absolute;
 z-index: -1;
 max-height: ${props => props.isMobile ? '250px' : '475px'};
 left: ${props => props.isMobile ? '-55px' : '0px'};
-margin-top: ${props => props.isMobile ? '130px' : '50px'};
+margin-top: ${props => props.isMobile ? '140px' : '50px'};
 `
 
 export const LeftSide = styled.div`
